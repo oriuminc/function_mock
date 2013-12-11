66326d60cc3d21c5b526cf9d99d166331354b3a0
Ben Leung <ben_leungut@yahoo.com>
2013-12-10 19:01:55 -0500

    Added comments and cleaned up internal structures for parameterized stubs, resetting, and exposed functions.

--- function_mock/function_mock.php
+++ function_mock/function_mock.php
@@ -1,12 +1,11 @@
 <?php
 
-define('FUNCTION_NAME_REGEX', '/(\w+)\s*\(.*\)/'); // any string that ends with a "("
-define('FUNCTION_REGEX', '/\w+\((?<My_Group>(?:\([^()]*\)|.)*)\)/'); // any string that ends with a "("
-
 define('TOKEN_CODE', 0);
 define('TOKEN_VALUE', 1);
 define('SPACE_CODE', 371);
 
+define('DEFAULT_STUB_VALUE', 'default_stub_value');
+define('NO_TOKEN_FOUND_CODE', -999);
 
 /**
  * Class that supports stubbing functions.
@@ -15,62 +14,76 @@ class FunctionMock
 {
   private static $stubFunctionList = array();
 
-  public static function getStubbedFunctionList() {
-    return self::$stubFunctionList;
-  }
-
   /**
    * Returns a stubbed value based on the function name.
    *
-   * @param $function_name
+   * @param $functionName
    *   The name of the function to retrieve the stubbed value from.
    * @return
    *   The stubbed value for the function, if it exists. Otherwise it
    *   will throw an exception asking a stubbed value to be defined.
    * @throws StubMissingException
    */
-  public static function getStubbedValue($function_name) {
-    if (array_key_exists($function_name, self::$stubFunctionList)) {
-      return self::$stubFunctionList[$function_name]['stub_value'];
+  public static function getStubbedValue($functionName, $paramList = NULL) {
+    if (array_key_exists($functionName, self::$stubFunctionList)) {
+      if (self::paramListSpecificStubExists($functionName, $paramList)) {
+        return self::$stubFunctionList[$functionName][serialize($paramList)];
+      } else {
+        return self::$stubFunctionList[$functionName][DEFAULT_STUB_VALUE];
+      }
     } else {
-      throw new StubMissingException($function_name);
+      throw new StubMissingException($functionName);
     } 
   }
 
-
-  /** 
-   * Stubs a function to return a set value.
-   *
-   * @param $function_name
-   *   The name of the function to retrieve the stubbed value from.
-   * @return
-   *   The stubbed value for the function, if it exists. Otherwise it
-   *   will throw an exception asking a stubbed value to be defined.
-   * @throws StubMissingException
-   */
-  public static function stub($function_name, $return_value) {
-    self::$stubFunctionList[$function_name]['stub_value'] = $return_value;
+  private static function paramListSpecificStubExists($functionName, $paramList) {
+    return $paramList !== NULL && array_key_exists(serialize($paramList), self::$stubFunctionList[$functionName]);    
   }
 
-  public static function setDefaultStubValue($function_name) {
-    self::$stubFunctionList[$function_name]['stub_value'] = $stubFunctionList[$function_name]['default_return_value'];
+  /** 
+   * Sets up a stub value for a given mock function.
+   *
+   * @param $functionName
+   *   The name of the function to retrieve the stubbed value from.
+   * @param $returnValue
+   *   The value that you want returned when the function is called.
+   * @param $paramList
+   *   Optional array of parameters you want an exact match on, so you can 
+   *   do a conditional stub.
+   */
+  public static function stub($functionName, $returnValue, $paramList = NULL) {
+    if ($paramList !== NULL) {
+      // Make a key out of the $paramList array, by simply serializing it into a string.
+      self::$stubFunctionList[$functionName][serialize($paramList)] = $returnValue;
+    } else {
+      self::$stubFunctionList[$functionName][DEFAULT_STUB_VALUE] = $returnValue;
+    }
   }
 
   /**
    * Resets all the stubbed functions.
    */
   public static function resetStubs() {
-    foreach (self::$stubFunctionList as &$stubFunctionListItem) {
-      unset($stubFunctionListItem['stub_value']);
-    }
+    // Just empty the array.
+    self::$stubFunctionList = array();
   }
 
-
+  /**
+   * Finds the nearest previous token that doesn't contain a space as the value.
+   *
+   * @param &$tokens
+   *   Array of PHP language tokens to search.
+   * @param $i
+   *   Index to start searching from. The function will search for the next item from this point.
+   * @return
+   *   The value of the next non space token, or an array with a TOKEN_CODE of NO_TOKEN_FOUND_CODE is
+   *   returned.
+   */
   private static function findPrevNonSpaceToken(&$tokens, $i) {
     $counter = $i - 1;
 
     if ($counter <= 0) {
-      return array(TOKEN_CODE => T_FUNCTION);
+      return array(TOKEN_CODE => NO_TOKEN_FOUND_CODE);
     }
 
     while ($tokens[$counter][TOKEN_CODE] === SPACE_CODE &&
@@ -81,11 +94,22 @@ class FunctionMock
     return $tokens[$counter];
   }
 
+  /**
+   * Finds the next token that doesn't contain a space as the value.
+   *
+   * @param &$tokens
+   *   Array of PHP language tokens to search.
+   * @param $i
+   *   Index to start searching from. The function will search for the next item from this point.
+   * @return
+   *   The value of the next non space token, or an array with a TOKEN_CODE of NO_TOKEN_FOUND_CODE is
+   *   returned.
+   */
   private static function findNextNonSpaceToken(&$tokens, $i) {
     $counter = $i + 1;    
 
     if ($counter >= count($tokens)) {
-      return array(TOKEN_CODE => T_FUNCTION);
+      return array(TOKEN_CODE => NO_TOKEN_FOUND_CODE);
     }
 
     while ($tokens[$counter][TOKEN_CODE] === SPACE_CODE &&
@@ -97,79 +121,61 @@ class FunctionMock
   }
 
   /**
-   * Returns all functions that would require stubs (based on a list of source files to search holistically).
-   */
-  public static function findFunctionsNeedingStubs($src_files) {
+   * Returns the names of all the functions that would require 
+   * mocks to be defined (based on a list of source files to search holistically).
+   *
+   * @param $srcFiles
+   *   List of source files to consider. Note: In performing the search these files will be loaded and executed.
+   * @return
+   *   Array of function names that will need to be mocked.
+   */  
+  public static function findFunctionsNeedingMocks($srcFiles) {
     // Loop through all the source files to find the ones that need to be searched.
-    $completeFile = self::loadFiles($src_files);
-
-    // Use a text-based regex search first to get an initial set. It won't be perfectly accurate (it includes function
-    // calls that may be in comments, for example).
-    // preg_match_all(FUNCTION_REGEX, $completeFile, $matches);
-
-    // var_export($matches);
-
-    // $names = preg_grep(FUNCTION_NAME_REGEX, $matches[0]);
-    
-    // var_export($names);
-
-    // $matches2 = preg_grep(FUNCTION_REGEX, $matches[1]);
-
-    // var_export($matches2);
-
-    // $function_call_like_strings = array_unique($matches[1]);
+    $completeFile = self::loadFiles($srcFiles);
 
     // Do a token based search next based on the loaded files, which is closer but picks up some 
     // strings that don't entirely match.
     $tokens = token_get_all($completeFile);
-
-    // file_put_contents('block_tokens.txt', $tokens);
 
     $result = array();
 
     for ($i = 0; $i < count($tokens); $i++) {
       $token = $tokens[$i];
 
-      // if ($i + 1 > count($tokens) - 1 || $i - 1 < 0) {
-      //   continue;
-      // }
-
       if ($token[TOKEN_CODE] !== T_STRING) {
         continue;
       }
 
-      $next_token = FunctionMock::findNextNonSpaceToken($tokens, $i);
+      $nextToken = self::findNextNonSpaceToken($tokens, $i);
 
-      // var_export("previous \r\n");
-      // var_export($prev_token);
-      // var_export("current \r\n");
-      // var_export($token);
-      // var_export("next \r\n");
-      // var_export($next_token);
-
-      if ($next_token[TOKEN_CODE] !== '(') {
+      if ($nextToken[TOKEN_CODE] !== '(') {
           // Not a function call.
           continue;
       }
 
-      $prev_token = FunctionMock::findPrevNonSpaceToken($tokens, $i);
+      $prevToken = self::findPrevNonSpaceToken($tokens, $i);
 
-      if ($prev_token[TOKEN_CODE] === T_FUNCTION) {
+      if ($prevToken[TOKEN_CODE] === NO_TOKEN_FOUND_CODE) {
           // It's a function definition, not a function call.
           continue;
       }
 
-      if ($prev_token[TOKEN_CODE] === T_OBJECT_OPERATOR) {
+      if ($prevToken[TOKEN_CODE] === T_FUNCTION) {
+          // It's a function definition, not a function call.
+          continue;
+      }
+
+      if ($prevToken[TOKEN_CODE] === T_OBJECT_OPERATOR) {
           // It's a method invocation, not a function call.
           continue;
       }
 
-      if ($prev_token[TOKEN_CODE] === T_DOUBLE_COLON) {
+      if ($prevToken[TOKEN_CODE] === T_DOUBLE_COLON) {
           // It's a static method invocation, not a function call.
           continue;
       }
 
-      if ($prev_token[TOKEN_CODE] === T_NEW) {
+      if ($prevToken[TOKEN_CODE] === T_NEW) {
           // It's a class instantiation.
           continue;
       }
@@ -178,147 +184,70 @@ class FunctionMock
       $result[] = $token[TOKEN_VALUE];
     }
 
-    // var_export($result);
-
-    // $string_tokens = array_unique(
-    //     array_map(
-    //         function($t) { return $t[1]; }, 
-    //         array_filter($tokens, function($t) { return $t[0] == T_STRING; })
-    //     )
-    //   );
-
-    // var_export($string_tokens);
-    
-
-    // // Then, do an intersection of both results to get the best set of functions (minus commented ones)
-    // // and unnecessary string elements.
-    // $function_list = array_intersect(array_unique($string_tokens), $function_call_like_strings);
-
-    // Look for functions that do not exist.
+    // Look for functions that do not exist. These are the ones that need to be mocked.
     $result = array_filter($result, function($function) { return !function_exists(trim($function)); });  
 
     return $result;
   }
 
-  // public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
-  //   {
-
-  //       $tokens       = $phpcsFile->getTokens();
-  //       $functionName = $tokens[$stackPtr]['content'];
-
-  //       // Find the next non-empty token.
-  //       $openBracket = $phpcsFile->findNext(PHP_CodeSniffer_Tokens::$emptyTokens, ($stackPtr + 1), null, true);
-
-  //       if ($tokens[$openBracket]['code'] !== T_OPEN_PARENTHESIS) {
-  //           // Not a function call.
-  //           return;
-  //       }
-
-  //       if (isset($tokens[$openBracket]['parenthesis_closer']) === false) {
-  //           // Not a function call.
-  //           return;
-  //       }
-
-  //       // Find the previous non-empty token.
-  //       $search   = PHP_CodeSniffer_Tokens::$emptyTokens;
-  //       $search[] = T_BITWISE_AND;
-  //       $previous = $phpcsFile->findPrevious($search, ($stackPtr - 1), null, true);
-  //       if ($tokens[$previous]['code'] === T_FUNCTION) {
-  //           // It's a function definition, not a function call.
-  //           return;
-  //       }
-
-  //       if ($tokens[$previous]['code'] === T_OBJECT_OPERATOR) {
-  //           // It's a method invocation, not a function call.
-  //           return;
-  //       }
-
-  //       if ($tokens[$previous]['code'] === T_DOUBLE_COLON) {
-  //           // It's a static method invocation, not a function call.
-  //           return;
-  //       }
-
-  //       $this->phpcsFile    = $phpcsFile;
-  //       $this->functionCall = $stackPtr;
-  //       $this->openBracket  = $openBracket;
-  //       $this->closeBracket = $tokens[$openBracket]['parenthesis_closer'];
-  //       $this->arguments    = array();
-
-  //       $phpcsFile->addWarning(
-  //               'Function Call ' . $tokens[$stackPtr]['content'],
-  //               $stackPtr
-  //           );
-
-  //   }//end process()  
-
-  public static function loadFiles($src_files) {
+  /**
+   * Load and include files into the PHP context.
+   *
+   * @param $srcFiles
+   *   List of source files to load.
+   * @return
+   *   Combined contents of all the files and their PHP code loaded and executed. Note if any errors occur on
+   *   any specific file being loaded, it will be ignored.
+   */  
+  public static function loadFiles($srcFiles) {
     $result = '';
-    foreach ($src_files as $src_file) {
-      include_once $src_file;
-      $result .= file_get_contents($src_file);
+    foreach ($srcFiles as $srcFile) {
+      include_once $srcFile;
+      $result .= file_get_contents($srcFile);
     }
 
     return $result;
   }
 
-  // private static function generateRandomString($length = 5) {
-  //     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  //     $randomString = '';
-  //     for ($i = 0; $i < $length; $i++) {
-  //         $randomString .= $characters[rand(0, strlen($characters) - 1)];
-  //     }
-  //     return $randomString;
-  // }
-
   /**
    * Generate actual function definitions that can be stubbed, based on an array of function names.
-   */
-  public static function createStubbableFunctionDefinitions($result) {
+   *
+   * @param $functionList
+   *   List of functions to create mocks for.
+   * @return
+   *   Combined contents of all PHP code for mock functions created based on the function list.
+   */  
+  public static function createMockFunctionDefinitions($functionList) {
     $functionString = '';
-    foreach ($result as $item) {
-      // $tempFile = FunctionMock::generateRandomString().".php";
-
-      // var_export($tempFile);
-      
-      $newFunctionDefinition = 'function ' . $item . '() { return FunctionMock::getStubbedValue(__FUNCTION__); } ';
+    
+    foreach ($functionList as $item) {      
+      $newFunctionDefinition = 'function ' . $item . '() { return FunctionMock::getStubbedValue(__FUNCTION__, count(func_get_args()) > 0 ? func_get_args() : NULL); } ';
 
       if (!function_exists($item)) {
         eval($newFunctionDefinition);
       }
-      // Create a temporary file so you can run include_once on it. This is safer than eval(), since if it already exists
-      // it throws a PHP fatal error that unrecoverable. There might be situations that a function definition is attempted
-      // more than once, depending on the file being parsed.
-      // file_put_contents($tempFile, $newFunctionDefinition);
-
-      // Try including it once.
-      // include $tempFile;
-
-      // Delete the file now.
-      //unlink(realpath($tempFile));
 
       $functionString .= $newFunctionDefinition;
     }
-
-    // // Evaluate the actual function definitions, therefore making them real. The risk of doing this is that sometimes
-    // // you will end up redefining a function if it already exists (if you are running this within the same
-    // // thread), since you can't necessarily "undefine" it as a teardown event. This is the side effect of not generating a file
-    // // and using include_once on it).
-    // try {
-    //   eval($functionString);
-    // } catch (Exception $e) {
-    //   // Ignore for now.
-    // }
 
     return $functionString;
   }
 
   /**
-   * Generate and execute stubbable functions based on a list of files.
-   */
-  public static function generateStubbableFunctions($src_files) {
-    $function_list = self::findFunctionsNeedingStubs($src_files);
+   * Finds and generates mock functions based on a set of source files provided.
+   *
+   * Any functions that are not already PHP functions or declared within the scope of 
+   * the passed source files list will be auto-generated with mocks.
+   *
+   * @param $srcFiles
+   *   List of source files to consider for creating mock functions.
+   * @return
+   *   Combined contents of all PHP code for mock functions created based on the function list.
+   */ 
+  public static function generateMockFunctions($srcFiles) {
+    $functionList = self::findFunctionsNeedingMocks($srcFiles);
 
-    return self::createStubbableFunctionDefinitions($function_list);
+    return self::createMockFunctionDefinitions($functionList);
   }
 }
 
@@ -329,12 +258,11 @@ class StubMissingException extends Exception
 {
   public function __construct($functionName, $code = 0, Exception $previous = null) {
       $this->message = $functionName . ' has not been stubbed yet.' . "\r\n" .
-        'Please call stub(\'' . $functionName . '\', <stub_value>) to set a value.';
+        'Please call FunctionMock::stub(\'' . $functionName . '\', <stub_value>) to set a value.';
  
       parent::__construct($this->message, $code, $previous);
   }
 
-  // Return the string value.
   public function __toString() {
     return $this->message;
   }
--- function_mock/test/function_mock.test.php
+++ function_mock/test/function_mock.test.php
@@ -4,34 +4,34 @@ require_once '../function_mock.php';
 
 class FunctionMockTest extends PHPUnit_Framework_TestCase
 {
-  public function testResetStubs() {
-    FunctionMock::resetStubs();
 
-    $stubFunctionList = FunctionMock::getStubbedFunctionList();
+  public function testCreateMockFunctionDefinitions() {
+    // Generate a stub method dynamically and ensure you can call it.
+    $result = FunctionMock::createMockFunctionDefinitions(array('test_method1', 'test_method2'));
 
-    // Check that the array has no stubbed values.
-    foreach ($stubFunctionList as &$stubFunctionListItem) {
-        $this->assertEquals(false, array_key_exists('stub_value', $stubFunctionListItem));
-    }        
+    $this->assertEquals('function test_method1() { return FunctionMock::getStubbedValue(__FUNCTION__, count(func_get_args()) > 0 ? func_get_args() : NULL); } ' . 
+      'function test_method2() { return FunctionMock::getStubbedValue(__FUNCTION__, count(func_get_args()) > 0 ? func_get_args() : NULL); } ', $result);
+
+    FunctionMock::stub('test_method1', 3);
+
+    $this->assertEquals(3, test_method1());
   }
 
   public function testStub() {
-    // Stub a function and verify it shows what you need.
-
     // Set up the test input.
     $testStubValue = 3;
-    $functionName = 'abc';
+    $functionName = 'testStub';
 
+    FunctionMock::createMockFunctionDefinitions(array($functionName));
     FunctionMock::stub($functionName, $testStubValue);
 
     // Check that it sets it correctly.
-    $stubFunctionList = FunctionMock::getStubbedFunctionList();
-    $this->assertEquals($testStubValue, $stubFunctionList[$functionName]['stub_value']);
+    $this->assertEquals($testStubValue, testStub());    
   }
 
   public function testGetStubbedValueForStub() {
     $testStubValue = 3;
-    $functionName = 'abc';
+    $functionName = 'test';
 
     FunctionMock::stub($functionName, $testStubValue);
 
@@ -40,55 +40,92 @@ class FunctionMockTest extends PHPUnit_Framework_TestCase
     $this->assertEquals($testStubValue, $actualResult);
   }
 
+  public function testStubWithParameters() {
+    // Set up the test input.
+    $testStubValue = 3;
+    $testStubValueWithParam = 5;
+    $functionName = 'testStubParams';
+    $paramList = array('param1', 'param2');
+
+    FunctionMock::createMockFunctionDefinitions(array($functionName));
+    FunctionMock::stub($functionName, $testStubValue);
+    FunctionMock::stub($functionName, $testStubValueWithParam, $paramList);
+
+    // Check that it sets it correctly.
+    $this->assertEquals($testStubValue, testStubParams());    
+    $this->assertEquals($testStubValueWithParam, testStubParams('param1', 'param2'));    
+  }
+
   /**
    * @expectedException        StubMissingException
    * @expectedExceptionMessage nonExistentStub has not been stubbed yet.
    */
-  public function testGetStubbedValueForNonExistentStub() {
+  public function testGetStubbedValueForNonExistentMock() {
     $actualResult = FunctionMock::getStubbedValue('nonExistentStub');
-
-    $this->assertEquals($testStubValue, $actualResult);
   }
 
   public function testGetUniqueFunctions() {
-    $result = FunctionMock::findFunctionsNeedingStubs(array('./test_php.php'));
+    $result = FunctionMock::findFunctionsNeedingMocks(array('./test_php.php'));
 
     $this->assertEquals(7, count($result));
   }
 
-  public function testCreateStubbableFunctionDefinitions() {
+  public function testGenerateMockFunctions() {
     // Generate a stub method dynamically and ensure you can call it.
-    $result = FunctionMock::createStubbableFunctionDefinitions(array('test_method1', 'test_method2'));
-
-    $this->assertEquals('function test_method1() { return FunctionMock::getStubbedValue(__FUNCTION__); } ' . 
-        'function test_method2() { return FunctionMock::getStubbedValue(__FUNCTION__); } ', $result);
-
-    FunctionMock::stub('test_method1', 3);
-
-    $this->assertEquals(3, test_method1());
-  }
-
-  public function testGenerateStubbableFunctions() {
-    // Generate a stub method dynamically and ensure you can call it.
-    FunctionMock::generateStubbableFunctions(array('./test_php.php'));
+    FunctionMock::generateMockFunctions(array('./test_php.php'));
 
     FunctionMock::stub('abc', 3);
 
     $this->assertEquals(3, abc('http://abc.com/get'));
   }
 
-  // public function testDrupalBlockModuleFunctions() {
-  //   // Generate a stub method dynamically and ensure you can call it.
-  //   $result = FunctionMock::generateStubbableFunctions(array('./block.module'));
+  public function testResetStubs() {
+    // Stub a few functions, then reset them and see that nothing shows up.
+    $testStubValue = 3;
+    $functionName = 'testResetStub';
 
-  //   $page = array();
-  //   block_page_build($page);
+    FunctionMock::createMockFunctionDefinitions(array($functionName));
+    FunctionMock::stub($functionName, $testStubValue);
 
-  //   print_r($result);
+    // Test first that the stub exists and works.
+    $this->assertEquals($testStubValue, testResetStub());
 
-  //   // $this->assertEquals(3, drupal_http_request('http://abc.com/get'));
+    // Then, reset the stubs and make sure it doesn't return anything.
+    FunctionMock::resetStubs();
 
-  // }
+    // Test first that the stub exists and works.
+    try {
+      testResetStub();
+      $this->fail('Should have thrown a StubMissingException');
+    } catch (StubMissingException $e) {
+      // Expected behavior.
+      return;
+    }
+  }
+
+  public function testDrupalBlockModuleFunctions() {
+    // Generate a stub method dynamically and ensure you can call it.
+    $result = FunctionMock::generateMockFunctions(array('./block.module'));
+
+    // The concept here is to assume that all the Drupal based functions work 
+    // as intended, and simply control the return values to what you'd expect
+    // in your test scenario. This gives you control to test different scenarios
+    // and verify the results.
+    $page = array();
+    global $theme;
+
+    $theme = 'abc';
+
+    FunctionMock::stub('drupal_theme_initialize', '');
+    FunctionMock::stub('system_region_list', array());
+    FunctionMock::stub('menu_get_item', array('path' => 'abc'));
+    FunctionMock::stub('drupal_static_reset', '');
+
+    block_page_build($page);
+
+    // $this->assertEquals(3, drupal_http_request('http://abc.com/get'));
+
+  }
 
 }
 ?>
