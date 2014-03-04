a75e90a8ae5ab2c6aeda6c62344d24a7fa3d89aa
Ihor Marusyk <ih2502mk@gmail.com>
2014-03-04 18:15:33 -0500

    Adopting whitespace token for different versions of php

--- function_mock.php
+++ function_mock.php
@@ -7,6 +7,9 @@ define('SPACE_CODE', 371);
 define('DEFAULT_STUB_VALUE', 'default_stub_value');
 define('NO_TOKEN_FOUND_CODE', -999);
 
+$s = token_get_all(" ");
+define('T_WHITESPACE', $s[0][0]);
+
 /**
  * Class that supports stubbing functions.
  */
